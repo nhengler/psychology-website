15ffeb86f1f3706ea6bfb5c879a89b5f299e0998
Nicolas Hengler <niko_las1934@hotmail.com>
2026-05-04 06:33:50 -0300

    button fix

--- apps/web/src/views/marketing/about.tsx
+++ apps/web/src/views/marketing/about.tsx
@@ -1,5 +1,5 @@
 import { Heart, BookOpen, Users, ArrowRight } from "lucide-react";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const values = [
