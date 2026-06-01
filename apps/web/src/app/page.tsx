d9a2580f53535ee6b290d245f5bfe93885ea879f
unknown <niko_las1934@hotmail.com>
2026-06-01 05:09:47 -0300

    folders organization

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Header } from "@/views/header"
-import { Hero } from "@/views/hero"
-import { About } from "@/views/about"
-import { Services } from "@/views/services"
-import { Benefits } from "@/views/benefits"
-import { Materials } from "@/views/materials"
-import { Appointments } from "@/views/appointments"
-import { FAQ } from "@/views/faq"
-import { Footer } from "@/views/footer"
+import { Header } from "@/components/header"
+import { Hero } from "@/components/hero"
+import { About } from "@/components/about"
+import { Services } from "@/components/services"
+import { Benefits } from "@/components/benefits"
+import { Materials } from "@/components/materials"
+import { Appointments } from "@/components/appointments"
+import { FAQ } from "@/components/faq"
+import { Footer } from "@/components/footer"
 
 export default function Home() {
   return (
